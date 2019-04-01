5be55ed59351bffd1925ac95ec516ce6e54546e1
venqka <venelinborisov5@gmail.com>
2019-04-01 09:26:17 +0300

    Initial commit

---
+++ woo-widget-areas.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Woo Widget Areas
+Plugin URI: devnecks.com
+Description: This plugin adds widget areas to WooCommerce product pages
+Author: venqka
+Author URI: devnecks.com
+Version: 1.0
+*/
+
