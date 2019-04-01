9fdfcac9e6d7105cf12a0eaacdfcd99730a93fb0
venqka <venelinborisov5@gmail.com>
2019-04-01 11:16:56 +0300

    Enqueue styles

--- woo-widget-areas.php
+++ woo-widget-areas.php
@@ -10,3 +10,10 @@ Version: 1.0
 
 include( 'woo-sidebars.php' );
 include( 'woo-hooks.php' );
+
+function wwa_enqueue() {
+	/*frontend styles*/
+	wp_enqueue_style( 'wwa-style', plugin_dir_url( __FILE__ ) . 'style.css', array(), '1.0', false );
+
+}
+add_action( 'wp_enqueue_scripts', 'wwa_enqueue' );
