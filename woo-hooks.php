6a7e8296cea2dde7a1ef793087de17748fdf09ee
venqka <venelinborisov5@gmail.com>
2019-04-01 11:09:01 +0300

    Hook the single product before add to cart widget area

--- woo-hooks.php
+++ woo-hooks.php
@@ -9,3 +9,12 @@ function product_category_before_content_widget_area() {
 	}
 }
 add_action( 'woocommerce_before_main_content', 'product_category_before_content_widget_area' );
+
+function single_product_before_add_to_cart_button() {
+
+	is_product() {
+		dynamic_sidebar( 'product-category-before-content' );
+	}
+		
+}
+add_action( 'woocommerce_before_add_to_cart_button', 'single_product_before_add_to_cart_button' );
