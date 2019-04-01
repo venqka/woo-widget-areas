9f59394729a62c797a5d02a5ac04d3dff4552693
venqka <venelinborisov5@gmail.com>
2019-04-01 11:05:43 +0300

    Create single product before add to cart button widget area

--- woo-sidebars.php
+++ woo-sidebars.php
@@ -14,4 +14,17 @@ $product_category_before_content_args = array(
 );
 register_sidebar( $product_category_before_content_args );
 
+//Product Category before content
+$single_product_before_add_to_cart_button_args = array(
+	'name'          => __( 'Single product before add to cart button', 'dn' ),
+	'id'            => 'product-category-before-content',
+	'description'   => __( 'This widget area is placed before the add to cart button on single product', 'dn' ),
+	'class'         => '',
+	'before_widget' => '<li id="%1$s" class="widget %2$s product-category-before-content">',
+	'after_widget'  => '</li>',
+	'before_title'  => '<h2 class="widgettitle">',
+	'after_title'   => '</h2>' 
+);
+register_sidebar( $single_product_before_add_to_cart_button_args );
+
 
