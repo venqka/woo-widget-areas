bcef79f42f13c8e2b33b0aa153ede7d80d4dc56f
venqka <venelinborisov5@gmail.com>
2019-04-02 12:09:04 +0300

    Create single product after add to cart sidebar

--- woo-sidebars.php
+++ woo-sidebars.php
@@ -14,7 +14,7 @@ $product_category_before_content_args = array(
 );
 register_sidebar( $product_category_before_content_args );
 
-//Product Category before content
+//Single product before add to cart
 $single_product_before_add_to_cart_button_args = array(
 	'name'          => __( 'Single product before add to cart button', 'dn' ),
 	'id'            => 'single-product-before-add-to-cart',
@@ -26,5 +26,18 @@ $single_product_before_add_to_cart_button_args = array(
 	'after_title'   => '</h2>' 
 );
 register_sidebar( $single_product_before_add_to_cart_button_args );
+
+//Single product after add to cart
+$single_product_after_add_to_cart_button_args = array(
+	'name'          => __( 'Single product after add to cart button', 'dn' ),
+	'id'            => 'single-product-after-add-to-cart',
+	'description'   => __( 'This widget area is placed after the add to cart button on single product', 'dn' ),
+	'class'         => '',
+	'before_widget' => '<li id="%1$s" class="widget %2$s wwa single-product-after-add-to-cart">',
+	'after_widget'  => '</li>',
+	'before_title'  => '<h2 class="widgettitle">',
+	'after_title'   => '</h2>' 
+);
+register_sidebar( $single_product_after_add_to_cart_button_args );
 
 
